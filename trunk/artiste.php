b5134436469fa0d72bed235fc2c2ddbbeb9992e2
sylvain.brunerie@gmail.com <sylvain.brunerie@gmail.com@2f16fd11-2b3b-c7d2-ac13-b177cae26f20>
2012-05-20 11:38:28 +0000

    Modif artiste.php (tous si pas id)

--- trunk/artiste.php
+++ trunk/artiste.php
@@ -2,13 +2,26 @@
 function echoArtiste()
 {
 	echo '
-			<div id="contenu" class="cArtiste">
-				';
-	$nomArtiste = "Artiste bidon";
+			<div id="contenu" class="cArtiste">';
+			
+	if(isset($_GET["idArtiste"]) and is_numeric($_GET["idArtiste"]) and $_GET["idArtiste"] > 0)
+	{
+		$nomArtiste = "Artiste bidon";
 	
-	echo '
+		echo '
 				<h2 id="nomArtiste">Artiste : '.$nomArtiste.'</h2>
 				<p>Désolé, pas plus d’informations pour l’instant.</p>
 			</div>';
+	}
+	else
+	{
+		// Liste de tous les artistes
+		echo '
+				<h2>Liste de tous les artistes enregistrés</h2>
+				<p>(Bientôt)</p>';
+	}
+	echo '
+			</div>
+			';
 }
 ?>
