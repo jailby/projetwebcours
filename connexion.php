99e2d40c0aa3e6a778dbc4c8b068749f13152fcb
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-05-23 08:38:40 +0000

    Connexion Venus

--- connexion.php
+++ connexion.php
@@ -3,11 +3,6 @@
 function connexion()
 {
 	global $c;	
-	# $utilisateurDB = "jbsubils"; 
-	# $utilisateurDB = "sbrunerie";
-	$utilisateurDB = "root";
-
-	$passwordDB = $utilisateurDB;
 	
 	// Sur mon ordi j’ai configuré MySQL comme ça :
 	//     login : root
@@ -15,18 +10,35 @@ function connexion()
 	//     base de données : glin607
 	// Faudra donc que tu fasses pareil (ou du moins qu’on utilise les mêmes identifiants sur nos deux ordis).
 	
+	// echo "<pre>"; var_dump($_SERVER); echo "</pre>";
+	
 	if($_SERVER["HTTP_HOST"] == "localhost")
 	{
-		$c = mysql_connect("localhost", $utilisateurDB, $passwordDB) or die("Erreur connect : ".mysql_error());
-		mysql_select_db("glin607",$c) or die ("Erreur select_db : ".mysql_error());
+		if(strpos($_SERVER["SERVER_SOFTWARE"], "CentOS") !== false) 
+		// on est à la fac, serveur SQL « Venus »
+		{
+			$utilisateurDB = "sbrunerie";
 		
+			$c = mysql_connect("venus", $utilisateurDB, $utilisateurDB) or die("Erreur connect : ".mysql_error());
+			mysql_select_db("$utilisateurDB",$c) or die ("Erreur select_db : ".mysql_error());
+		}
+		else // on est en local
+		{
+			$utilisateurDB = "root";
+		
+			$c = mysql_connect("localhost", $utilisateurDB, $utilisateurDB) or die("Erreur connect : ".mysql_error());
+			mysql_select_db("glin607",$c) or die ("Erreur select_db : ".mysql_error());
+		}
 	}
 	elseif($_SERVER["HTTP_HOST"] == "innsbay.toile-libre.org")
 	{
 		$c = mysql_connect("sql.toile-libre.org", "innsbay_projweb", "GLIN607") or die("Erreur connect : ".mysql_error());
 		mysql_select_db("innsbay_projweb",$c) or die ("Erreur select_db : ".mysql_error());
 	}
-	else {} // rajouter cas de la fac/venus éventuellement
+	else
+	{
+		echo "<p>Erreur : sur quel serveur est-on ?</p>";
+	}
 	
 	mysql_set_charset("utf8", $c) or die ("Erreur set_charset utf8 : ".mysql_error());
 }
