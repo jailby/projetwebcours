fa0d3cbb3ac0f9b15f868b766df46c42138c1fd8
sylvain.brunerie@gmail.com <sylvain.brunerie@gmail.com@2f16fd11-2b3b-c7d2-ac13-b177cae26f20>
2012-05-31 07:38:06 +0000

    Liste des albums d’un artiste + affichage pseudo conditionnel

---
+++ trunk/fonctions.php
@@ -0,0 +1,35 @@
+<?php
+
+function echoErreurP()
+{
+	if(isset($_GET["p"]))
+	{
+		echo '
+			<div id="contenu" class="cErreurP">
+				<p>Désolé, le paramètre «&nbsp;'.$_GET["p"].'&nbsp;» n’a pas été reconnu.</p>
+			</div>';
+	}
+	else
+	{
+		echoAccueil();
+	}
+}
+
+function echoLicence()
+{
+	echo '
+			<div id="contenu" class="licence">
+				<p>Tout le contenu de ce site est mis à disposition sous la licence Creative Commons <a href="http://creativecommons.org/licenses/by-sa/2.0/fr/">CC-BY-SA</a> (créditer Sylvain Brunerie et Jean-Baptiste Subils).</p>
+				<p>Oui, même la musique !</p>
+			</div>';
+}
+
+function echoMentions()
+{
+	echo '
+			<div id="contenu" class="mentions">
+				<p>(Il faut mettre quoi dans les mentions légales ?)</p>
+			</div>';
+}
+
+?>
