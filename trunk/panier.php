460cacf432f239df7d5262eea2bbb90d32e48578
jbsubils@gmail.com <jbsubils@gmail.com@2f16fd11-2b3b-c7d2-ac13-b177cae26f20>
2012-06-01 07:33:07 +0000

    finit le calcul du prix dans panier

--- trunk/panier.php
+++ trunk/panier.php
@@ -23,20 +23,28 @@ function echoPanier()
 		{
 			while ($ligneAlbum = mysql_fetch_assoc($albums))
 			{
+
 				$p = 0;
 				$cpt = 0;
 				$titresAlbums = mysql_query("SELECT * FROM titresalbums 
 											WHERE IdAlbum=".$ligneAlbum["IdAlbum"]) 
 								or die ( "Erreur requete album");
-				while ($ligneTitre = mysql_fetch_assoc($idTitres))
+				
+				while ($ligneTitreAlbum = mysql_fetch_assoc($titresAlbums))
 				{
-					while ($ligneTitreAlbum = mysql_fetch_assoc($titresAlbums))
+					while ($ligneTitre = mysql_fetch_assoc($idTitres))
 					{
 						if ($ligneTitre["IdTitre"] == $ligneTitreAlbum["IdTitre"])
 						{
-							$p += 0;
+							$p += $ligneTitre["Prix"];
+							$cpt++;
 						}
 					}
+				}
+				if (mysql_num_rows($titresAlbums) == $cpt)
+				{
+					echo ' T as une reduc !!!!!!!!!!!';
+					$prix -= $p*0.1;
 				}
 				mysql_data_seek($idTitres,0);
 			}
@@ -46,7 +54,7 @@ function echoPanier()
 			mysql_data_seek($idTitres,0);
 			while  ($ligneTitre = mysql_fetch_assoc($idTitres))
 			{
-				$prix = $prix + 1; // !!!!!!!!!!!!!!!!!! à remplacer par le prix du titre
+				$prix = $prix + $ligneTitre["Prix"]; // !!!!!!!!!!!!!!!!!! à remplacer par le prix du titre
 				echo'
 					<tr><td>'.$ligneTitre["NomTitre"].'</td><td>'.$ligneTitre["Prix"].'</td></tr>';
 			
