d74402c52a0c1b3106292f987fc65494c43b4ae7
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-06-01 07:38:53 +0000

    Supprimer sélection panier

--- panier.php
+++ panier.php
@@ -2,22 +2,44 @@
 
 function echoPanier()
 {
+	echo '
+			<div id="contenu" class="cPanier">';
+	
 	if(isset($_SESSION["utilisateurNom"]))
 	{
 		$user = $_SESSION["utilisateurId"];
 		$prix = 0;
 		
+		if(isset($_REQUEST["vider"]))
+		{
+			$reqViderPanier = mysql_query("DELETE FROM panier WHERE IdUtilisateur=".$user) or die ("Erreur vidage panier");
+		}
+		
+		if(isset($_REQUEST["supprimePanier"])) // on supprime des éléments du panier
+		{
+			// var_dump($_REQUEST);
+			$reqTitresPanier = mysql_query("SELECT * FROM panier WHERE IdUtilisateur = ".$user) or die ("Erreur nom titres panier");
+			while($ligneTitre = mysql_fetch_assoc($reqTitresPanier))
+			{
+				if(isset($_REQUEST["c".$ligneTitre["IdTitre"]]))
+				{
+					$reqSupprimePanier = mysql_query("DELETE FROM panier WHERE IdTitre = ".$ligneTitre["IdTitre"]." AND IdUtilisateur = ".$user."") or die ("Erreur ajout titre au panier");
+				}
+			}
+		}
+				
 		$idTitres  = mysql_query("SELECT * FROM titres,panier 
 					WHERE panier.IdUtilisateur =".$user."
 					AND titres.IdTitre = panier.IdTitre") 
 					or die ("Erreur récupération des titres"); 
 		
 		$albums = mysql_query("SELECT * FROM albums") or die ( "Erreur requete album");
+
 		
 		if (mysql_num_rows($idTitres) == 0)
 		{
 			echo ' 
-				<p> Votre panier est vide</p>';
+				<p>Votre panier est vide.</p>';
 		}
 		else
 		{
@@ -49,26 +71,50 @@ function echoPanier()
 				mysql_data_seek($idTitres,0);
 			}
 			echo '
-				<p> Voici les Chanson dans votre panier :<p>
-				<table>';
+				<p> Voici les chansons dans votre panier : <p>
+				<form method="POST">
+					<table>
+						<thead>
+						<tr>
+							<th>Titre</th>
+							<!--<th>Artiste</th>-->
+							<th>Prix</th>
+						</tr>
+						</thead>
+						<tbody>';
 			mysql_data_seek($idTitres,0);
 			while  ($ligneTitre = mysql_fetch_assoc($idTitres))
 			{
 				$prix = $prix + $ligneTitre["Prix"]; // !!!!!!!!!!!!!!!!!! à remplacer par le prix du titre
 				echo'
-					<tr><td>'.$ligneTitre["NomTitre"].'</td><td>'.$ligneTitre["Prix"].'</td></tr>';
+						<tr>
+							<td><i>'.$ligneTitre["NomTitre"].'</i></td>
+							<!--<td></td>-->
+							<td>'.$ligneTitre["Prix"].' €</td>
+							<td><input type="checkbox" class="checkTitre" name="c'.$ligneTitre["IdTitre"].'" /></td>
+						</tr>';
 			
 			}
 			echo '
-				</table>
-				<p> Voici le montant que vous avez à payer : '.$prix.'<br\>';
+					</tbody>
+					</table>
+					<p class="cocher">
+						<a href="javascript:toutCocherDecocher();" rel="cocher" id="bCocher">Tout cocher / Tout décocher</a> 
+					</p>
+					<input type="submit" value="Supprimer la sélection du panier" name="supprimePanier" id="supprimePanier" />
+					<p> Montant total à payer : '.$prix.' € </p>
+					<input type="submit" name="vider" value="Vider le panier" />
+				</form>';
 		}
 		
 	}
 	else
 	{
-		echo '<h2> Il faut être connecté pour avoir un panier </h2>';
+		echo '
+				<h2> Il faut être connecté pour avoir un panier </h2>';
 	}
+	echo '
+			</div>';
 }
 
 function calculPrix()
